dbd56128e7aabe93b82b0b012715f7c1358f8915
imul22 <33175066+imul22@users.noreply.github.com>
2025-12-18 10:56:33 +0900

    update port

--- app.js
+++ app.js
@@ -102,8 +102,14 @@ app.post('/api/hitung-tangki', (req, res) => {
         longTon: longTon.toFixed(3)
     });
 });
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server berjalan di http://localhost:${port}`);
+    });
+}
+module.exports = app; // Sangat penting untuk Vercel
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 //app.listen(port, () => console.log(`Server aktif di http://localhost:${port}`));
